a7afbe4343fe6c57aea4cbf47bf83143318e4f73
Mohnish Singh <mohnishsr555@gmail.com>
2025-08-25 00:01:51 +0530

    Create std1.js

---
+++ std1.js
@@ -0,0 +1,18 @@
+// student.js
+
+// Create a student object
+const student = {
+    name: "Mohnish Singh",
+    age: 20,
+    branch: "Artificial Intelligence & Data Science",
+    rollNo: "AIDS123",
+    college: "RGPV"
+};
+
+// Print student details
+console.log("----- Student Details -----");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Branch: " + student.branch);
+console.log("Roll No: " + student.rollNo);
+console.log("College: " + student.college);
